a8dac1d19a107dca230bcc3aad0923e01f30efbd
Ryan Corazza Alvarenga <ryancorazzaalvarenga@gmail.com>
2025-11-17 22:31:46 -0300

    Contribuição aos CRUDS

--- Vinicola-app/Vinicola-app/DAO/DataLoggerDAO.cs
+++ Vinicola-app/Vinicola-app/DAO/DataLoggerDAO.cs
@@ -1,6 +1,36 @@
-﻿namespace Vinicola_app.DAO
+﻿using Vinicola_app.DAO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+using Vinicola_app.Models;
+
+namespace Vinicola_app.DAO
 {
     public class DataLoggerDAO
     {
+        private SqlParameter[] NewParameters(DataLoggerViewModel dataLogger)
+        {
+            SqlParameter[] p = new SqlParameter[8];
+            p[0] = new SqlParameter("id", dataLogger.Id);
+            p[1] = new SqlParameter("wineryId", dataLogger.WineryId);
+            p[2] = new SqlParameter("userId", dataLogger.UserId);
+            p[3] = new SqlParameter("tempMin", dataLogger.TempMin);
+            p[4] = new SqlParameter("tempMax", dataLogger.TempMax);
+            p[5] = new SqlParameter("lumMin", dataLogger.LumMin);
+            p[6] = new SqlParameter("lumMax", dataLogger.LumMax);
+            p[7] = new SqlParameter("humidMin", dataLogger.HumidMin);
+            p[8] = new SqlParameter("humidMax", dataLogger.HumidMax);
+
+        }
+
+        public void Insert(DataLoggerViewModel dataLogger)
+        {
+            string sql = "insert into dataLogger(id, wineryId, userId, tempMin, tempMax, lumMin, lumMax, humidMin, humidMax) values (@id, @winery_id, @user_id, @temp_min, @temp_max, @lum_min, @lum_max, @humid_min, @humid_max)";
+            HelperDAO.ExecutaSQL(sql, NewParameters(dataLogger));
+        }
+
+
     }
 }
--- Vinicola-app/Vinicola-app/Models/DataLoggerViewModel.cs
+++ Vinicola-app/Vinicola-app/Models/DataLoggerViewModel.cs
@@ -2,5 +2,16 @@
 {
     public class DataLoggerViewModel
     {
+        public int Id { get; set; }
+
+        public int WineryId { get; }
+        public int UserId { get; }
+
+        public double TempMin { get; set; }
+        public double TempMax { get; set; }
+        public double LumMin { get; set; }
+        public double LumMax { get; set; }
+        public double HumidMin { get; set; }
+        public double HumidMax { get; set; }
     }
 }
