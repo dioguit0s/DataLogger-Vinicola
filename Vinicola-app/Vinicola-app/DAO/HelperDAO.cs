a0b7f4f862883a822aed42a52232bffb99da976a
leonardorosario <leobr4417@gmail.com>
2025-11-18 22:27:05 -0300

    feat: Cria CRUD Winery

--- Vinicola-app/Vinicola-app/DAO/HelperDAO.cs
+++ Vinicola-app/Vinicola-app/DAO/HelperDAO.cs
@@ -33,6 +33,36 @@ namespace Vinicola_app.DAO
                 }
             }
         }
+
+        public static void ExecutaProc(string nomeProc, SqlParameter[] parametros)
+        {
+            using (SqlConnection conexao = ConexaoBD.GetConexao())
+            {
+                using (SqlCommand comando = new SqlCommand(nomeProc, conexao))
+                {
+                    comando.CommandType = CommandType.StoredProcedure;
+                    if (parametros != null)
+                        comando.Parameters.AddRange(parametros);
+                    comando.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public static DataTable ExecutaProcSelect(string nomeProc, SqlParameter[] parametros)
+        {
+            using (SqlConnection conexao = ConexaoBD.GetConexao())
+            {
+                using (SqlDataAdapter adapter = new SqlDataAdapter(nomeProc, conexao))
+                {
+                    if (parametros != null)
+                        adapter.SelectCommand.Parameters.AddRange(parametros);
+                    adapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+                    DataTable tabela = new DataTable();
+                    adapter.Fill(tabela);
+                    return tabela;
+                }
+            }
+        }
     }
 
 }
